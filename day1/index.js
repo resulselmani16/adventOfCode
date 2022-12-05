22447f25590398b1407102997ae9d0eaba75fc62
Resul Selmani <resul@frakton.com>
2022-12-05 23:08:49 +0100

    day1 and fileReadTemplate

---
+++ day1/index.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const path = require("path");
+const readline = require("readline");
+
+const filePath = path.resolve(__dirname, "input.txt");
+
+const inputStream = fs.createReadStream(filePath);
+
+let maxCalories = 0;
+let caloriesPerPerson = [];
+let currentHigh = 0;
+let personWithMostCalories = 0;
+
+var lineReader = readline.createInterface({
+  input: inputStream,
+  terminal: false,
+});
+
+lineReader.on("line", (line) => {
+  if (line !== "") {
+    caloriesPerPerson.push(Number(line));
+    for (var i = 0; i < caloriesPerPerson.length; i++) {
+      currentHigh += caloriesPerPerson[i];
+      if (maxCalories < currentHigh) {
+        personWithMostCalories++;
+        maxCalories = currentHigh;
+        console.log(maxCalories)
+        console.log(personWithMostCalories, "personWithMostCalories");
+      }
+    }
+    currentHigh = 0;
+  } else {
+    caloriesPerPerson = [];
+  }
+});
