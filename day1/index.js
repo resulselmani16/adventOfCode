5ddad67de0c7dd6da2405b74efc4db4e6de49fe3
Resul Selmani <resul@frakton.com>
2022-12-08 15:47:56 +0100

    remove unnecessary logs

--- day1/index.js
+++ day1/index.js
@@ -19,19 +19,14 @@ lineReader.on("line", (line) => {
   calories.push(+line);
   if (line) {
     num += +line;
-    console.log(num);
   } else {
     caloriesPerPerson.push(num);
-    console.log(caloriesPerPerson);
     num = 0;
   }
 });
 
 lineReader.on("close", () => {
-  console.log(calories);
-  console.log(caloriesPerPerson);
   caloriesPerPerson.push(calories[calories.length - 1])
   const topThree = caloriesPerPerson.sort((a, b) => a - b)
-  console.log(topThree);
   console.log(topThree.slice(-3).reduce((a, b) => a + b))
 ;});
